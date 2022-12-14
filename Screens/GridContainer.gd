e896b65d2a439f12a0b6fd131ee975484008b086
Pike <pikentk@gmail.com>
2022-12-14 13:22:24 -0500

    stuff

--- Screens/GridContainer.gd
+++ Screens/GridContainer.gd
@@ -5,12 +5,17 @@ extends GridContainer
 # var a = 2
 # var b = "text"
 
+var inv = [];
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
+func _addLetter(l):
+	
+	$".".add_child(new Label)
+	inv.append(l);
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
