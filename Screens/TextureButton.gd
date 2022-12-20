784f124ef367ea124debab60bae17069b5aa859d
Rebecca Lee <rlee1@ucvts.org>
2022-12-20 13:46:05 -0500

    These are my changes. It should just be the settings menu

--- Screens/TextureButton.gd
+++ Screens/TextureButton.gd
@@ -1,11 +1,4 @@
 extends Control
 
-
- 
 func _on_Button_pressed():
-	get_tree().change_scene("res://Screens/GameScene.tscn")
-	
-
-
-func _on_PlayButton2_pressed():
 	get_tree().change_scene("res://Screens/GameScene.tscn")
