24e554ae587e6aab8356d8a267c6f1386ae5996b
Gooey64 <61029550+Gooey64@users.noreply.github.com>
2023-01-20 02:25:32 -0500

    Added pause menu, not everything works though

--- Screens/Pause.gd
+++ Screens/Pause.gd
@@ -1,6 +1,21 @@
 extends Button
 
-onready var _tpm = $"../../PauseMenu"
+onready var _tpm = $"../../PauseMenuNode/PauseMenu"
+
+var is_paused = false setget set_is_paused
+
+func set_is_paused(value):
+	is_paused = value
+	get_tree().paused = is_paused
 
 func _on_Pause_pressed():
+	print("The pause button is being pressed.")
+	self.is_paused = !is_paused
 	_tpm.popup()
+
+
+func _on_Resume_pressed():
+	print("The resume button is being pressed.")
+	if _tpm == null:
+		print("_tpm is null")
+	self.is_paused = false
