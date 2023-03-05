3d09aea70339903cd011fa5ff9415f3bfc2acd13
Gooey64 <61029550+Gooey64@users.noreply.github.com>
2023-03-05 01:23:27 -0500

    Fixed the exit tutorial button

--- Screens/SceneButtons.gd
+++ Screens/SceneButtons.gd
@@ -10,3 +10,9 @@ onready var transition = self.owner.get_node("Transition")
 func _on_ExitButton_pressed():
 	$"../../ButtonClickSFX".play();
 	transition.transition_in("res://Screens/MainScene.tscn");
+
+
+
+func _on_TutorialExit_pressed():
+	$"../../../ButtonClickSFX".play();
+	transition.transition_in("res://Screens/MainScene.tscn");
