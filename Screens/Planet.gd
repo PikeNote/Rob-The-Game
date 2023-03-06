72e5ecd5725a7e07dcd389ce13dd1cbe1fa78678
Pike <pikentk@gmail.com>
2023-03-05 20:36:48 -0500

    Level Passed Indicator; Level Three Fix
    - Fixed a bug with level 3 crashing when the game ends
    - Added an indicator to show if a level has been passed or not
    - Deleted a duplicate LevelThree scene

--- Screens/Planet.gd
+++ Screens/Planet.gd
@@ -13,19 +13,29 @@ onready var robScale = $"../RobSideProfile".scale.x;
 onready var transition = $"../Transition";
 onready var leftButton = $"../LevelSelect/LevelPanel/ButtonContainer/VBoxContainer/MapSelectButtons/LeftButton";
 onready var rightButton = $"../LevelSelect/LevelPanel/ButtonContainer/VBoxContainer/MapSelectButtons/RightButton"
+
+var checkedBox = load("res://Assets/MainMenu/Checked-Box.PNG")
+var uncheckedBox = load("res://Assets/MainMenu/Unchecked-Box.PNG")
+
+# Checkpoitns around the planet in which Rob stops at
 # Rotations are in degrees
 var rotationalCheckpoints = [-42.9,69.4,214.7];
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# Reset Rob's animation back to the start to stop him from floating
 	$"../AnimationPlayer".play("JumpingRob");
 	$"../AnimationPlayer".seek(0,true);
 	$"../AnimationPlayer".stop(true);
+	
+	# Go to the scene the user was on; changes dynamically as the current scene variable changes based on what level the user
+	# went to on the level select
 	_doneMoving(GameReferences.currentScene);
 	$".".rotation_degrees = rotationalCheckpoints[GameReferences.currentScene]
 	
 func _physics_process(delta):
 	if(moving):
+		# Dotation forward/backwards based on if Rob is rotated/flipped
 		if($"../RobSideProfile".scale.x > 0):
 			$".".rotation_degrees += 0.5;
 			
@@ -49,10 +59,15 @@ func _pauseLoop():
 
 
 func _moveNextLocation(movingBackwards:bool=false):
+	# Remove the visiblity of the level select panels
 	$"../LevelSelect".visible = false;
 	moving = true;
+	
+	# Start animaton
 	$"../AnimationPlayer".get_animation("JumpingRob").loop = true;
 	$"../AnimationPlayer".play("JumpingRob")
+	
+	# Flip Rob if moving backwards
 	if(!movingBackwards):
 		moveTo = GameReferences.currentScene + 1;
 	else:
@@ -60,6 +75,7 @@ func _moveNextLocation(movingBackwards:bool=false):
 	
 
 func _doneMoving(index):	
+	# Set title and description of the level info panels
 	$"../LevelSelect/LevelPanel/Headers/Title".bbcode_text = "[center]%s[/center]" % GameParameters.levelDescription[index].Name;
 	$"../LevelSelect/LevelPanel/Description".bbcode_text = "[center]%s[/center]" % GameParameters.levelDescription[index].Description;
 	
@@ -75,6 +91,13 @@ func _doneMoving(index):
 	
 	var requirements = GameParameters.levelDescription[index].Requirements;
 	$"../LevelSelect/LevelPanel/Headers/LevelRequirements/Requirements".bbcode_text = "[center] Spell %s words - Get %s points[/center]" % [requirements.spelled,requirements.points];
+	
+	
+	if(GameParameters.levelDescription[index].Name in UserManager.settings.levelsCompleted):
+		$"../LevelSelect/LevelPanel/LevelPassed/Checkbox".texture = checkedBox;
+	else:
+		$"../LevelSelect/LevelPanel/LevelPassed/Checkbox".texture = uncheckedBox;
+	
 	
 	moving = false;
 	GameReferences.currentScene = index;
@@ -108,6 +131,7 @@ func _on_RightButton_pressed():
 	$"../RobSideProfile".scale.x=robScale*-1;
 	_moveNextLocation(true);
 
+# Stop animation if Rob/planet is no longer movnig
 func _on_AnimationPlayer_animation_finished(anim_name):
 	if !moving:
 		$"../AnimationPlayer".stop(true);
