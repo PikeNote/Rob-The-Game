82d1e847e1cee0c4aad013f6326e36958439cce0
Pike <pikentk@gmail.com>
2023-03-13 09:38:55 -0400

    Button Fix
    - Fixed audio click not referencing the proper SFX

--- Screens/PageChange.gd
+++ Screens/PageChange.gd
@@ -4,13 +4,13 @@ var pageIndex = 0;
 onready var maxPageIndex = $"../../Pages".get_child_count();
 
 func _on_LeftClick_pressed():
-	$"../../../ButtonClickSFX".play();
+	AudioController._playClick();
 	if(pageIndex - 1 >= 0):
 		pageIndex -= 1;
 	pageShow();
 	
 func _on_RightClick_pressed():
-	$"../../../ButtonClickSFX".play();
+	AudioController._playClick();
 	if(pageIndex + 1 < maxPageIndex):
 		pageIndex += 1;
 	pageShow();
